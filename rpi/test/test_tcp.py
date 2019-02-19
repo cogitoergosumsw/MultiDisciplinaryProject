3c031e212fe665b0d3c803cee46bad0b9232d440
Seng Wee <>
2019-02-19 11:37:00 +0800

    add tcp class main file

--- rpi/test/test_tcp.py
+++ rpi/test/test_tcp.py
@@ -1,37 +1,37 @@
 import socket, time
 
 IP_ADDRESS = "192.168.18.18"
-IP_PORT = 12316
+IP_PORT = 12318
+
+class Test_TCPCon:
+
+    def __init__(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.Tcp_connect(IP_ADDRESS, IP_PORT)
+
+    def Tcp_connect(self, HostIp, Port):
+        self.s.connect((HostIp, Port))
+        return "Connected!"
 
 
-def Tcp_connect(HostIp, Port):
-    global s
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HostIp, Port))
-    return "Connected!"
+    def Tcp_Write(self, D):
+        D = D.encode()
+        self.s.send(D)
+        return
 
 
-def Tcp_Write(D):
-    D = D.encode()
-    s.send(D)
-    return
+    def Tcp_Read(self):
+        temp = ' '
+        msg_read = ''
+        while temp != '\r':
+            temp = self.s.recv(1024)
+            temp = temp.decode()
+            msg_read = msg_read + temp
+            print("msg received: %s" % msg_read)
+        return msg_read
 
+    def Tcp_Close(self):
+        self.s.close()
+        return
 
-def Tcp_Read():
-    a = ' '
-    b = ''
-    while a != '\r' or a != '':
-        a = s.recv(1024)
-        a = a.decode()
-        b = b + a
-    return b
-
-
-def Tcp_Close():
-    s.close()
-    return
-
-Tcp_connect(IP_ADDRESS, IP_PORT)
-while 1:
-    print("Reading from PC || %s", Tcp_Read())
 
